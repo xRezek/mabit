34985eab87866db2fd2ef5cb6946b46c6f1bdd0a
Mikołaj Maj <mmaj02@interia.pl>
2025-02-01 23:27:27 +0100

    Add SQL queries for OEE parameters, quality metrics, and machine data retrieval

---
+++ sqlQueries.php
@@ -0,0 +1,9 @@
+<?php
+
+  $sqlQueryGetOEEparams = "SELECT avg(quality), avg(availability), avg(effectiveness) FROM daily_data"; //* zapydanie obliczające średnią wartość dla wszystkich wskaźników OEE
+
+  $sqlGetQuality = "SELECT SUM(OK), SUM(NOK), SUM(ANULOWANY) FROM( SELECT CASE WHEN status = 1 THEN 1 ELSE 0 END AS OK, CASE WHEN status = 2 THEN 1 ELSE 0 END AS NOK, CASE WHEN status = 3 THEN 1 ELSE 0 END AS ANULOWANY FROM produkty) a"; //* zapytanie zlicza dobre, złe i anulowane produkty
+
+  $sqlGetDataForLinearChart = "SELECT date, ROUND(quality * 100,2) AS quality , ROUND(quality * availability,2) * 100 as OEE FROM daily_data;"; //* zapytanie oblicza wskaźnik OEE
+
+  $sqlGetMachines = "SELECT machineId FROM maszyny";

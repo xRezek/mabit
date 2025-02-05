c2d112883133dd9ce36fabb7aa8d17abc32be96c
Mikołaj Maj <mmaj02@interia.pl>
2025-02-05 22:40:09 +0100

    Add offcanvas component for alerts and events in offcanvas.php

---
+++ offcanvas.php
@@ -0,0 +1,11 @@
+<div class="offcanvas offcanvas-end offcanvas-custom-width" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="staticBackdropLabel">Alerty i eventy</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <div>
+      <p>I will not close if you click outside of me.</p>
+    </div>
+  </div>
+</div>

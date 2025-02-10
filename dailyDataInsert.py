93dbffb514ca45ed5568cd04bd0d7cbd4c12b1c4
Mikołaj Maj <mmaj02@interia.pl>
2025-02-10 09:56:08 +0100

    Refactor daily data insertion SQL for improved readability and add dataCollector.py for MQTT data handling

--- dailyDataInsert.py
+++ dailyDataInsert.py
@@ -14,27 +14,36 @@ def insert_daily_data():
     password=os.getenv("DB_PASSWORD"),
     database=os.getenv("DB_DATABASE")
   )
-  sqlInsertDailyData = """INSERT INTO daily_data (date, quality, availability, effectiveness)
-                          SELECT
-                            CURRENT_DATE() as date,
-                            (SELECT 
-                              ROUND(
-                                SUM(CASE WHEN status = 1 THEN 1 ELSE 0 END) / NULLIF(COUNT(*), 0), 
-                                2
-                              ) 
-                            FROM `produkty`
-                            WHERE status IN (1, 2, 3) AND DATE(timestamp) = CURRENT_DATE()) AS quality,
-                            (SELECT
-                              ROUND((SELECT (COUNT(*) * 20) / 3600 FROM machine_status
-                            WHERE isOn = 1 AND DATE(timestamp) = CURRENT_DATE()) 
-                            /
-                            (SELECT (COUNT(*) * 20) / 3600 FROM machine_status
-                            WHERE DATE(timestamp) = CURRENT_DATE()),2)) AS availability,
-                            (SELECT
-                              ROUND(AVG(COALESCE(scale, 0)) / 100,2) AS effectiveness
-                            FROM
-                              `produkty`
-                            WHERE DATE(timestamp) = CURRENT_DATE()) AS effectiveness;"""
+  sqlInsertDailyData = """
+    INSERT INTO daily_data (date, quality, availability, effectiveness)
+    SELECT
+      CURRENT_DATE() as date,
+      (
+        SELECT 
+          ROUND(
+            SUM(CASE WHEN status = 1 THEN 1 ELSE 0 END) / NULLIF(COUNT(*), 0), 
+            2
+          ) 
+        FROM `produkty`
+        WHERE status IN (1, 2, 3) AND DATE(timestamp) = CURRENT_DATE()
+      ) AS quality,
+      (
+        SELECT
+          ROUND(
+            (SELECT (COUNT(*) * 20) / 3600 FROM machine_status
+            WHERE isOn = 1 AND DATE(timestamp) = CURRENT_DATE()) 
+            /
+            (SELECT (COUNT(*) * 20) / 3600 FROM machine_status
+            WHERE DATE(timestamp) = CURRENT_DATE()), 2
+          )
+      ) AS availability,
+      (
+        SELECT
+          ROUND(AVG(COALESCE(scale, 0)) / 100, 2) AS effectiveness
+        FROM `produkty`
+        WHERE DATE(timestamp) = CURRENT_DATE()
+      ) AS effectiveness;
+  """
   
   if connection.is_connected():
     print("Connected to MySQL Server")
@@ -42,9 +51,9 @@ def insert_daily_data():
     mysql_cursor.execute(sqlInsertDailyData)
     connection.commit()
     print("Insert successfull")
+    mysql_cursor.close()
   else:
     print("Connection error")
-  mysql_cursor.close()
   connection.close()
 
 

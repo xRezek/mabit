6b2b8cca208568977f38a181335798160ae17313
Mikołaj Maj <mmaj02@interia.pl>
2025-02-12 12:28:44 +0100

    Refactor daily data insertion to use PyMySQL and improve error handling; remove debug.php file

--- dailyDataInsert.py
+++ dailyDataInsert.py
@@ -1,65 +1,74 @@
 import schedule
 import time
-import mysql.connector as mysql
+import pymysql
 import os
-from dotenv.main import load_dotenv
+from dotenv import load_dotenv
 
 load_dotenv()
 
-
 def insert_daily_data():
-  connection = mysql.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_DATABASE")
-  )
-  sqlInsertDailyData = """
-    INSERT INTO daily_data (date, quality, availability, effectiveness)
-    SELECT
-      CURRENT_DATE() as date,
-      (
-        SELECT 
-          ROUND(
-            SUM(CASE WHEN status = 1 THEN 1 ELSE 0 END) / NULLIF(COUNT(*), 0), 
-            2
-          ) 
-        FROM `produkty`
-        WHERE status IN (1, 2, 3) AND DATE(timestamp) = CURRENT_DATE()
-      ) AS quality,
-      (
-        SELECT
-          ROUND(
-            (SELECT (COUNT(*) * 20) / 3600 FROM machine_status
-            WHERE isOn = 1 AND DATE(timestamp) = CURRENT_DATE()) 
-            /
-            (SELECT (COUNT(*) * 20) / 3600 FROM machine_status
-            WHERE DATE(timestamp) = CURRENT_DATE()), 2
-          )
-      ) AS availability,
-      (
-        SELECT
-          ROUND(AVG(COALESCE(scale, 0)) / 100, 2) AS effectiveness
-        FROM `produkty`
-        WHERE DATE(timestamp) = CURRENT_DATE()
-      ) AS effectiveness;
-  """
-  
-  if connection.is_connected():
-    print("Connected to MySQL Server")
-    mysql_cursor = connection.cursor()
-    mysql_cursor.execute(sqlInsertDailyData)
-    connection.commit()
-    print("Insert successfull")
-    mysql_cursor.close()
-  else:
-    print("Connection error")
-  connection.close()
-
+  try:
+      connection = pymysql.connect(
+          host=os.getenv("DB_HOST"),
+          user=os.getenv("DB_USER"),
+          password=os.getenv("DB_PASSWORD"),
+          database=os.getenv("DB_DATABASE"),
+          port=int(os.getenv("DB_PORT", 3306)),
+          connect_timeout=10
+      )
+      print("Connected to MySQL Server (PyMySQL)")
+      
+      cursor = connection.cursor()
+      sqlInsertDailyData = """
+          INSERT INTO daily_data (date, quality, availability, effectiveness)
+          SELECT
+            CURRENT_DATE() as date,
+            (
+              SELECT 
+                ROUND(
+                  SUM(CASE WHEN status = 1 THEN 1 ELSE 0 END) / NULLIF(COUNT(*), 0), 
+                  2
+                ) 
+              FROM produkty
+              WHERE status IN (1, 2, 3) AND DATE(timestamp) = CURRENT_DATE()
+            ) AS quality,
+            (
+              SELECT
+                ROUND(
+                  (SELECT (COUNT(*) * 20) / 3600 FROM machine_status
+                    WHERE isOn = 1 AND DATE(timestamp) = CURRENT_DATE()) 
+                  /
+                  (SELECT (COUNT(*) * 20) / 3600 FROM machine_status
+                    WHERE DATE(timestamp) = CURRENT_DATE()), 
+                  2
+                )
+            ) AS availability,
+            (
+              SELECT
+                ROUND(AVG(COALESCE(scale, 0)) / 100, 2)
+              FROM produkty
+              WHERE DATE(timestamp) = CURRENT_DATE()
+            ) AS effectiveness;
+      """
+      
+      cursor.execute(sqlInsertDailyData)
+      connection.commit()
+      print("Insert successful")
+      cursor.close()
+      
+  except Exception as e:
+      print("Error inserting daily data:", e)
+      
+  finally:
+      try:
+          connection.close()
+          print("Connection closed")
+      except Exception as e:
+          print("Error closing connection:", e)
 
 schedule.every().day.at("23:00").do(insert_daily_data)
 
 while True:
-  print("Running schedule")
-  schedule.run_pending()
-  time.sleep(1)
+    print("Running schedule")
+    schedule.run_pending()
+    time.sleep(1)
--- dataCollector.py
+++ dataCollector.py
@@ -1,115 +1,131 @@
 import paho.mqtt.client as mqtt
-import mysql.connector as mysql
+import pymysql
 import json
 import os
-from dotenv.main import load_dotenv
+from dotenv import load_dotenv
 
 load_dotenv()
 
-
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
     client.subscribe("ZONE_01/#")
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-  # Parsing a JSON payload data.
-  try:
-    DATA = json.loads(msg.payload)
-  except json.JSONDecodeError:
-    print("Invalid JSON payload")
-    print("Payload: ", msg.payload)
-    return
-  
-  print("\n\n\n")
+    try:
+        data = json.loads(msg.payload)
+    except json.JSONDecodeError:
+        print("Invalid JSON payload")
+        print("Payload:", msg.payload)
+        return
 
-  if DATA is not None:
-    connection = mysql.connect(
-    host = os.getenv("DB_HOST"),
-    user = os.getenv("DB_USER"),
-    password = os.getenv("DB_PASSWORD"),
-    database = os.getenv("DB_DATABASE"),
-    )
+    print("\n\n\n")
 
-    if connection.is_connected():
-      print("Connected to MySQL Server")
-      mysql_cursor = connection.cursor()
+    if data is not None:
+        try:
+            # Nawiązanie połączenia przy użyciu PyMySQL
+            connection = pymysql.connect(
+                host=os.getenv("DB_HOST"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD"),
+                database=os.getenv("DB_DATABASE"),
+                port=int(os.getenv("DB_PORT", 3306)),
+                connect_timeout=10
+            )
+            print("Connected to MySQL Server (PyMySQL)")
+            cursor = connection.cursor()
 
-      if DATA["Alarm"]["message"] != "":
-        sql_insert_query_alarmy = "INSERT INTO alarmy(alarmId, machineId, message, code, timestamp) VALUES (NULL, %s, %s, %s, CURRENT_TIMESTAMP)"
-        alarm_values = (DATA["MachineID"], DATA["Alarm"]["message"], DATA["Alarm"]["code"])
-        mysql_cursor.execute(sql_insert_query_alarmy, alarm_values)
-        connection.commit()
-        print("Alarm inserted successfully")
-      else:
-        print("Alarm not inserted")
+            # Wstawianie alarmu
+            if data["Alarm"]["message"] != "":
+                sql_insert_query_alarmy = (
+                    "INSERT INTO alarmy(alarmId, machineId, message, code, timestamp) "
+                    "VALUES (NULL, %s, %s, %s, CURRENT_TIMESTAMP)"
+                )
+                alarm_values = (data["MachineID"], data["Alarm"]["message"], data["Alarm"]["code"])
+                cursor.execute(sql_insert_query_alarmy, alarm_values)
+                connection.commit()
+                print("Alarm inserted successfully")
+            else:
+                print("Alarm not inserted")
 
-      if DATA["Event"]["message"] != "":
-        sql_insert_query_events = "INSERT INTO events(eventId, machineId, code, message, timestamp) VALUES (NULL, %s, %s, %s, CURRENT_TIMESTAMP)"
-        events_values = (DATA["MachineID"], DATA["Event"]["code"], DATA["Event"]["message"])
-        mysql_cursor.execute(sql_insert_query_events, events_values)
-        connection.commit()
-        print("Event inserted successfully")
-      else:
-        print("Event not inserted")
-      
-      if DATA["product"]["Comment"] != "":
-        sql_insert_query_product = "INSERT INTO produkty (id, machineId, status, program, cycletime, progress, scale, timestamp) VALUES (%s, %s, %s, %s, %s, %s, %s, CURRENT_TIMESTAMP)"
-        product_values = (DATA["product"]["ID"], DATA["MachineID"], DATA["product"]["Status"], DATA["product"]["Serial"], DATA["product"]["CycleTime"], DATA["product"]["Comment"]["PROGRESS"], DATA["product"]["Comment"]["SCALE"])
-        mysql_cursor.execute(sql_insert_query_product, product_values)
-        connection.commit()
-        print("Product inserted successfully")
-      else:
-        print("Product not inserted")
+            # Wstawianie eventu
+            if data["Event"]["message"] != "":
+                sql_insert_query_events = (
+                    "INSERT INTO events(eventId, machineId, code, message, timestamp) "
+                    "VALUES (NULL, %s, %s, %s, CURRENT_TIMESTAMP)"
+                )
+                events_values = (data["MachineID"], data["Event"]["code"], data["Event"]["message"])
+                cursor.execute(sql_insert_query_events, events_values)
+                connection.commit()
+                print("Event inserted successfully")
+            else:
+                print("Event not inserted")
 
-      if DATA["MachineStatus"] is not None:
-        sql_insert_query_machine_status = "INSERT INTO machine_status (machineId, isOn, timestamp) VALUES (%s, %s, CURRENT_TIMESTAMP)"
-        machine_status_values = (DATA["MachineID"], DATA["MachineStatus"][0])
-        mysql_cursor.execute(sql_insert_query_machine_status, machine_status_values)
-        connection.commit()
-        print("Machine status inserted successfully")
+            # Wstawianie produktu
+            if data["product"]["Comment"] != "":
+                sql_insert_query_product = (
+                    "INSERT INTO produkty (id, machineId, status, program, cycletime, progress, scale, timestamp) "
+                    "VALUES (%s, %s, %s, %s, %s, %s, %s, CURRENT_TIMESTAMP)"
+                )
+                product_values = (
+                    data["product"]["ID"],
+                    data["MachineID"],
+                    data["product"]["Status"],
+                    data["product"]["Serial"],
+                    data["product"]["CycleTime"],
+                    data["product"]["Comment"]["PROGRESS"],
+                    data["product"]["Comment"]["SCALE"]
+                )
+                cursor.execute(sql_insert_query_product, product_values)
+                connection.commit()
+                print("Product inserted successfully")
+            else:
+                print("Product not inserted")
 
-      if DATA["OperatorID"] != "" or None:
-        sql_unique_machineid_query = "SELECT COUNT(*) FROM maszyny WHERE machineId = %s"
-        unique_value = (DATA["MachineID"],)
-        mysql_cursor.execute(sql_unique_machineid_query, unique_value)
-        result_unique = mysql_cursor.fetchone()
+            # Wstawianie statusu maszyny
+            if data.get("MachineStatus") is not None:
+                sql_insert_query_machine_status = (
+                    "INSERT INTO machine_status (machineId, isOn, timestamp) "
+                    "VALUES (%s, %s, CURRENT_TIMESTAMP)"
+                )
+                machine_status_values = (data["MachineID"], data["MachineStatus"][0])
+                cursor.execute(sql_insert_query_machine_status, machine_status_values)
+                connection.commit()
+                print("Machine status inserted successfully")
 
-        if result_unique[0] > 0:
-          print("Machine already exists")
-        else:
-          sql_insert_query_maszyny = "INSERT INTO maszyny (id, machineId, operatorId) VALUES (NULL, %s, %s)"
-          values = (DATA["MachineID"], DATA["OperatorID"])
-          mysql_cursor.execute(sql_insert_query_maszyny, values)
-          connection.commit()
+            # Wstawianie rekordu do tabeli 'maszyny' jeśli OperatorID nie jest pusty
+            if data.get("OperatorID", "") != "":
+                sql_unique_machineid_query = "SELECT COUNT(*) FROM maszyny WHERE machineId = %s"
+                unique_value = (data["MachineID"],)
+                cursor.execute(sql_unique_machineid_query, unique_value)
+                result_unique = cursor.fetchone()
 
-          if mysql_cursor.rowcount > 0:
-            print("Record inserted successfully to maszyny")
-          else:
-            print("Record not inserted")
+                if result_unique[0] > 0:
+                    print("Machine already exists")
+                else:
+                    sql_insert_query_maszyny = (
+                        "INSERT INTO maszyny (id, machineId, operatorId) VALUES (NULL, %s, %s)"
+                    )
+                    values = (data["MachineID"], data["OperatorID"])
+                    cursor.execute(sql_insert_query_maszyny, values)
+                    connection.commit()
+
+                    if cursor.rowcount > 0:
+                        print("Record inserted successfully to maszyny")
+                    else:
+                        print("Record not inserted")
+
+            cursor.close()
+            connection.close()
+            print("MySQL connection closed")
+        except pymysql.MySQLError as e:
+            print("MySQL Error:", e)
     else:
-      print("Connection failed")
-    
-    
-    mysql_cursor.close()
-    connection.close()
-    if not connection.is_connected():
-      print("MySQL connection is closed")
-    else:
-      print("MySQL connection is still open")
+        print("No data received")
 
-
-        
-# MQTT client setup
+# Konfiguracja klienta MQTT
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 
-# Connect to the MQTT broker
 mqttc.connect("broker.emqx.io", 1883, 60)
-
-# Blocking call to process network traffic, dispatch callbacks, and handle reconnecting
 mqttc.loop_forever()
